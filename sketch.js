9fd016095ffd839d6d79a5b462359eef39014f5e
andre starnman <andre.starnman@gmail.com>
2022-11-03 22:34:52 +0100

    grid and basic painting works

--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
 const containerEL = document.querySelector(".container");
+let canvasSize = 16;
 
-let multiplyNumber = 16;
+// Create columns/rows for sketchboard.
+
+function makeGrid() {
+  let gridSize = getSquares(canvasSize);
+
+  for (let i = 0; i < gridSize; i++) {
+    let square = document.createElement("div");
+    containerEL.appendChild(square).classList.add("board");
+    containerEL.style.cssText = `--canvas-size: ${canvasSize}`;
+  }
+}
+makeGrid();
+
+// add event listener to each cell and paint
+
+document.querySelectorAll(".board").forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "pink";
+  });
+});
+
+// Multiplier.
 
 function getSquares(number) {
   totalSquares = number * number;
   return totalSquares;
 }
-
-function makeGrid() {
-  let gridSize = getSquares(multiplyNumber);
-  for (let i = 0; i < gridSize; i++) {
-    let square = document.createElement("div");
-    containerEL.appendChild(square).classList.add("board");
-    containerEL.style.cssText = `--sketch-size: ${multiplyNumber}`;
-  }
-}
-makeGrid();
