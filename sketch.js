4acec84fddbf3fee751de24cf7b969f8acc90921
andre starnman <andre.starnman@gmail.com>
2022-11-04 15:57:56 +0100

    slider for resizable canvas working

--- sketch.js
+++ sketch.js
@@ -2,41 +2,41 @@ const containerEL = document.querySelector(".container");
 const sliderEL = document.getElementById("slider");
 const displaySize = document.getElementById("canvas-size");
 
-let canvasSize = 32;
-
 // slider for canvas size
 
-displaySize.innerText = sliderEL.value;
+makeCanvas(64);
+
+function makeCanvas(canvasSize) {
+  let size = Number(canvasSize) * Number(canvasSize);
+  containerEL.style.cssText = `--canvas-size: ${canvasSize}`;
+  for (let i = 0; i < size; i++) {
+    let square = document.createElement("div");
+    containerEL.appendChild(square).classList.add("board");
+  }
+  document.querySelectorAll(".board").forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "pink";
+    });
+  });
+}
+
+// Slider functionality
+
+displaySize.innerText = sliderEL.value; // show initial slider value
+
+sliderEL.addEventListener("mouseup", changeCanvasSize); // Change size of canvas
+
+// display current slider value
 
 sliderEL.oninput = function () {
   displaySize.innerText = this.value;
-  canvasSize = this.value;
 };
 
-// Create columns & rows for canvas.
-
-function makeGrid() {
-  let gridSize = getSquares(canvasSize);
-
-  for (let i = 0; i < gridSize; i++) {
-    let square = document.createElement("div");
-    containerEL.appendChild(square).classList.add("board");
-    containerEL.style.cssText = `--canvas-size: ${canvasSize}`;
-  }
-}
-makeGrid();
-
-// add event listener to each cell and paint
-
-document.querySelectorAll(".board").forEach((item) => {
-  item.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "pink";
+// remove childnodes & reset Canvas
+function changeCanvasSize() {
+  document.querySelectorAll(".board").forEach((item) => {
+    item.remove();
   });
-});
-
-// Multiplier.
-
-function getSquares(number) {
-  totalSquares = number * number;
-  return totalSquares;
+  canvasSize = sliderEL.value; // attach slidervalue to canvasSize variable
+  makeCanvas(canvasSize); // invoke makeGrid with current Slider-value
 }
